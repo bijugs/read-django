49f24d5e889ca1b100ba3c888010886da41affc1
Biju Nair <gs.biju@gmail.com>
2018-11-05 13:44:36 -0500

    Refactor templates, add links and static resources

--- readit/settings.py
+++ readit/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'readit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'readit', 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'readit', 'static'),
+)
